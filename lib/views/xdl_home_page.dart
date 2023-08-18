7fb444b93f2ccf90970e520cf728d266df525c6f
Mopilani - Nexapros Softwares <mopilani1@gmail.com>
2023-08-18 23:17:07 +0200

    Add XDLHomePage

---
+++ lib/views/xdl_home_page.dart
@@ -0,0 +1,76 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:uis_by_mopi/views/cars_shop_view.dart';
+import 'package:uis_by_mopi/views/course_view.dart';
+import 'package:uis_by_mopi/views/courses_app.dart';
+import 'package:uis_by_mopi/views/login_screen.dart';
+import 'package:uis_by_mopi/views/shoes_shoping_app_home.dart';
+
+import 'downloads_view.dart';
+import 'view_xdl_servers.dart';
+
+class XDLHomePage extends StatefulWidget {
+  const XDLHomePage({super.key});
+
+  @override
+  State<XDLHomePage> createState() => _HomePageState();
+}
+
+class Item {
+  Item(this.route, this.title);
+  Widget route;
+  String title;
+}
+
+class _HomePageState extends State<XDLHomePage> {
+  List<Item> pages = [
+    Item(
+      const ViewXDLServers(),
+      'View XDL Servers',
+    ),
+    Item(
+      const DownloadsView(),
+      'Downloads View',
+    ),
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('XDL App Home Page')),
+      body: GridView.builder(
+        gridDelegate:
+            const SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 5),
+        itemCount: pages.length,
+        itemBuilder: (context, index) {
+          var page = pages[index];
+          return InkWell(
+            child: Card(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                children: [
+                  const Icon(
+                    Icons.pages_rounded,
+                    size: 50,
+                  ),
+                  Text(
+                    page.title,
+                    style: const TextStyle(fontSize: 22),
+                  ),
+                ],
+              ),
+            ),
+            onTap: () {
+              Navigator.push(
+                context,
+                CupertinoPageRoute(
+                  builder: (context) => page.route,
+                ),
+              );
+            },
+          );
+        },
+      ),
+    );
+  }
+}
