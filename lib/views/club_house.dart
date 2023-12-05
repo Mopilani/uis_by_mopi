3655d7e0099f04a5120eafb4a38b30ddaa5b7d5e
Mopilani - Nexapros Softwares <mopilani1@gmail.com>
2023-12-05 07:55:58 +0200

    Remove some unused imports

--- lib/views/club_house.dart
+++ lib/views/club_house.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:google_fonts/google_fonts.dart';
 
 class ClubHouseView extends StatelessWidget {
   const ClubHouseView({super.key});
--- lib/views/instagram_view.dart
+++ lib/views/instagram_view.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:google_fonts/google_fonts.dart';
 
 class InstagramView extends StatelessWidget {
   const InstagramView({super.key});
